ecd023d70c314e7c5144ce8a641a981a46b72cd4
xiaonuo1326 <92018941+alazeprt@users.noreply.github.com>
2022-10-22 18:40:45 +0800

    Delete Test File
    Delete Test File

--- src/main/java/test.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class test {
-    public static void main(String[] args) throws IOException {
-        LanguageReader a = new LanguageReader();
-        System.out.println(a.GetLanguage());
-    }
-}
